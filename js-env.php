dcd8cd17416b99dab0da2fab323e3ec8f6d30015
Stefan Nyman <stefan@jumpstarter.io>
2015-06-30 13:44:34 +0200

    js-env: refactor to allow for multiple env paths

--- js-env.php
+++ js-env.php
@@ -1,51 +1,55 @@
 <?php
 
-function js_get_env() {
+define("JS_ENV_PATH", "/app/env.json");
+define("JS_ENV_APC_KEY", "jumpstarter-env");
+
+function js_get_env($env_path = JS_ENV_PATH, $apc_key = JS_ENV_APC_KEY) {
     // Fetch parsed environment from request cache.
-    static $env = null;
-    if ($env !== null)
-        return $env;
+    static $envs = array();
+    if (isset($envs[$env_path]) && $envs[$env_path] !== null)
+        return $envs[$env_path];
     // Fetch parsed environment from server cache.
     if (function_exists("apc_fetch")) {
-        $key = "jumpstarter-env";
-        $env = apc_fetch($key);
-        if (is_array($env))
+        $env = apc_fetch($apc_key);
+        if (is_array($env)) {
+            $envs[$env_path] = $env;
             return $env;
+        }
     }
     // Read environment and parse it.
-    $env = json_decode(file_get_contents("/app/env.json"), true);
+    $env = json_decode(file_get_contents($env_path), true);
     if (!is_array($env))
-        throw new Exception("could not parse /app/env.json (not jumpstarter container?)");
+        throw new Exception("could not parse $env_path (not jumpstarter container?)");
     // Store parsed environment in server cache.
     if (function_exists("apc_store")) {
-        $key = "jumpstarter-env";
-        apc_store($key, $env);
+        apc_store($apc_key, $env);
     }
+    $envs[$env_path] = $env;
     return $env;
 }
 
-function js_env_get_value($path) {
+function js_env_get_value($key_path, $env_path = JS_ENV_PATH, $apc_key = JS_ENV_APC_KEY) {
     static $cache = array();
-    $env = js_get_env();
-    if (isset($cache[$path]))
-        return $cache[$path];
-    $path_arr = explode(".", $path);
-    if (count($path_arr) == 0)
+    if (isset($cache[$env_path][$key_path]))
+        return $cache[$env_path][$key_path];
+    $env = js_get_env($env_path, $apc_key);
+    $path_arr = explode(".", $key_path);
+    if (count($path_arr) === 0)
         return null;
     $obj = $env;
     foreach($path_arr as $part) {
         if (!isset($obj[$part])) {
-            $cache[$path] = null;
+            $cache[$env_path][$key_path] = null;
             return null;
         }
         $obj = $obj[$part];
     }
-    $cache[$path] = $obj;
+    $cache[$env_path][$key_path] = $obj;
     return $obj;
 }
 
-function js_env_get_value_or_array($path) {
-    $obj = js_env_get_value($path);
+function js_env_get_value_or_array($path, $env_path = JS_ENV_PATH, $apc_key = JS_ENV_APC_KEY) {
+    $obj = js_env_get_value($path, $env_path, $apc_key);
     return is_array($obj)? $obj: array();
 }
 
